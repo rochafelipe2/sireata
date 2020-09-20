562889cb7d50e55c30f52745e579af8c1b5fa7e0
Felipe Rocha <lipdiso@gmail.com>
2020-09-20 17:56:42 -0300

    Criação da interface _InterfaceBO para servir de contrato para o BO, para as classes de BO respeitarem um contrato e não seguir nomeclaturas de métodos por criatividade do desenvolvedor

--- src/main/java/br/edu/utfpr/dv/sireata/bo/_InterfaceBO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/bo/_InterfaceBO.java
@@ -1,5 +1,14 @@
 package br.edu.utfpr.dv.sireata.bo;
 
-public interface _InterfaceBO {
+import java.util.List;
 
+
+import br.edu.utfpr.dv.sireata.model._InterfaceModel;
+
+public interface _InterfaceBO {
+	List<_InterfaceModel> listarTodos(_InterfaceModel filtro);
+	_InterfaceModel consultar(_InterfaceModel filtro);
+	_InterfaceModel consultar(int id);
+	boolean salvar(_InterfaceModel model);
+	boolean remover(int id);
 }
